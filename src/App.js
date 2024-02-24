33426143140b1a71333a8a5d672d5c7178a2a947
Konstantin <t777tt88@gmai.com>
2024-02-24 13:29:59 +0100

    last

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ProductsByCategoriesPage from './components/pages/ProductsByCategoriesPag
 import SingleProductPage from './components/pages/SingleProductPage/SingleProductPage';
 import CartPage from './components/pages/CartPage/CartPage';
 import NotFoundPage from './components/pages/NotFoundPage/NotFoundPage';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import AllSales from './components/pages/AllSales/AllSales';
 import { getAllcategories } from './requests/categories_req';
